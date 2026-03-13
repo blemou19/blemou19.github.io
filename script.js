10db797ff76036e3b4757bd7e42ee511e7cd1bc7
blemou19 <remybakaryblemou@gmail.com>
2026-03-13 03:14:59 +0100

    Add mobile responsive navigation

--- script.js
+++ script.js
@@ -68,3 +68,12 @@ renderer.render(scene,camera);
 }
 
 animate();
+
+/* ================= MOBILE MENU ================= */
+
+const toggle = document.getElementById("menu-toggle");
+const nav = document.getElementById("nav-menu");
+
+toggle.addEventListener("click", () => {
+nav.classList.toggle("active");
+});
